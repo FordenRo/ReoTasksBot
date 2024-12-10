c2844c1e0a4f52377178f43268fa8c4660ba3443
FordenRo <116118075+FordenRo@users.noreply.github.com>
2024-12-10 22:24:54 +0500

    Cleanup

--- globals.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from telebot.async_telebot import AsyncTeleBot
-
-
-engine = create_engine('sqlite:///:memory:', echo=True)
-session = Session(engine)
-bot = AsyncTeleBot('7667464125:AAGGh68G7OQuK_gVy9IBCORLC7afg-5fyKw', parse_mode='HTML', disable_web_page_preview=True)
-bot_name: str
