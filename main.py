a57a6114ce38f63ada423fa0ae5ecfbed68a211a
FordenRo <FordenRo@yandex.ru>
2024-12-12 00:11:17 +0500

    Refactoring

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from asyncio import run as run_async
+
 from aiogram import Dispatcher
-from handlers import start
-from globals import bot, session, engine
+
 from database import Base
+from globals import bot, session, engine
+from handlers import start
 
 
 async def main():
-    dispatcher = Dispatcher()
-    Base.metadata.create_all(engine)
+	dispatcher = Dispatcher()
+	Base.metadata.create_all(engine)
 
-    dispatcher.include_routers(start.router)
+	dispatcher.include_routers(start.router)
 
-    await dispatcher.start_polling(bot)
+	await dispatcher.start_polling(bot)
 
-    session.commit()
-    engine.dispose()
+	session.commit()
+	engine.dispose()
 
 
 if __name__ == '__main__':
-    run_async(main())
+	run_async(main())
